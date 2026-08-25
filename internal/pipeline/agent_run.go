BertramPetersen/gatehouse

fix(pipeline): bound every agent invocation by a timeout (#810)
* fix(pipeline): bound every agent invocation so a stall cannot hang a run

PR #807 only timed out Test; document, lint, rebase, PR, CI-fix, and auto-fix still called Agent.Run with an unbounded context. Enforce a 30m default at the shared RunAgent seam (and an executor backstop) so a stalled agent fails with a diagnostic instead of wedging the run.

* no-mistakes(document): Clarify agent timeout fallback behavior

* no-mistakes: apply CI fixes

internal/pipeline/agent_run.go
@@ -1,0 +1,143 @@
+package pipeline
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/kunchenguid/no-mistakes/internal/agent"
+	"github.com/kunchenguid/no-mistakes/internal/config"
+)
+
+// ErrAgentTimeout is the context cause used when the default per-invocation
+// agent deadline expires. Callers wrap it with a diagnostic that names the
+// budget; a late successful return after this cause is still a timeout.
+var ErrAgentTimeout = errors.New("agent timeout")
+
+// AgentTimeout is the per-invocation budget applied at the shared agent-run
+// seam. A positive Config.AgentTimeout wins; otherwise the default (30m).
+func AgentTimeout(cfg *config.Config) time.Duration {
+	if cfg != nil && cfg.AgentTimeout > 0 {
+		return cfg.AgentTimeout
+	}
+	return config.DefaultAgentTimeout
+}
+
+// RunAgent executes one agent invocation with a deadline scoped only to that
+// call. The parent StepContext.Ctx is left unchanged so post-agent work
+// (commits, git, parsing) is not cancelled by the invocation budget.
+//
+// If the parent context already has a deadline (review's round budget, Test's
+// explicit wrap, intent extraction, caller cancellation), that bound is
+// honored and no shorter default is stacked. Otherwise AgentTimeout is
+// applied. A late successful return after the deadline is rejected.
+func (sctx *StepContext) RunAgent(opts agent.RunOpts) (*agent.Result, error) {
+	parent := context.Background()
+	if sctx != nil {
+		parent = sctx.Ctx
+	}
+	return sctx.runAgent(parent, opts, "")
+}
+
+// RunAgentContext is RunAgent with an explicit parent, used when a step has
+// already installed a more specific deadline (review round, Test invocation).
+func (sctx *StepContext) RunAgentContext(parent context.Context, opts agent.RunOpts) (*agent.Result, error) {
+	return sctx.runAgent(parent, opts, "")
+}
+
+// RunAgentSessionContext is RunAgentSession with an explicit parent so a
+// fixer turn can share a round budget (review) or a per-invocation wrap (Test).
+func (sctx *StepContext) RunAgentSessionContext(parent context.Context, role SessionRole, opts agent.RunOpts) (*agent.Result, error) {
+	return sctx.runAgent(parent, opts, role)
+}
+
+func (sctx *StepContext) runAgent(parent context.Context, opts agent.RunOpts, sessionRole SessionRole) (*agent.Result, error) {
+	var ag agent.Agent
+	timeout := AgentTimeout(nil)
+	if sctx != nil {
+		ag = sctx.Agent
+		timeout = AgentTimeout(sctx.Config)
+	}
+	return invokeAgent(parent, timeout, func(ctx context.Context) (*agent.Result, error) {
+		if sessionRole != "" && sctx != nil && sctx.Sessions != nil {
+			return sctx.Sessions.Run(ctx, ag, sessionRole, opts, sctx.Log)
+		}
+		if ag == nil {
+			return nil, errors.New("nil agent")
+		}
+		return ag.Run(ctx, opts)
+	})
+}
+
+func invokeAgent(parent context.Context, timeout time.Duration, run func(context.Context) (*agent.Result, error)) (*agent.Result, error) {
+	ctx, cancel, applied := bindAgentDeadline(parent, timeout)
+	result, err := run(ctx)
+	runErr := classifyAgentRun(ctx, applied, err)
+	cancel()
+	if runErr != nil {
+		return nil, runErr
+	}
+	return result, nil
+}
+
+func bindAgentDeadline(parent context.Context, timeout time.Duration) (context.Context, context.CancelFunc, time.Duration) {
+	if parent == nil {
+		parent = context.Background()
+	}
+	if timeout <= 0 {
+		return parent, func() {}, 0
+	}
+	if _, ok := parent.Deadline(); ok {
+		return parent, func() {}, 0
+	}
+	ctx, cancel := context.WithTimeoutCause(parent, timeout, ErrAgentTimeout)
+	return ctx, cancel, timeout
+}
+
+func classifyAgentRun(ctx context.Context, applied time.Duration, err error) error {
+	if applied > 0 && errors.Is(context.Cause(ctx), ErrAgentTimeout) {
+		return fmt.Errorf("agent timed out after %s (agent silent for %s): %w", applied, applied, ErrAgentTimeout)
+	}
+	if cause := context.Cause(ctx); cause != nil {
+		return cause
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// timeoutAgent is the executor backstop: every sctx.Agent.Run is bounded even
+// if a future step forgets RunAgent. Nested with RunAgent it is a no-op when
+// the incoming context already has a deadline.
+type timeoutAgent struct {
+	inner   agent.Agent
+	timeout time.Duration
+}
+
+func (a *timeoutAgent) Name() string { return a.inner.Name() }
+
+func (a *timeoutAgent) Close() error { return a.inner.Close() }
+
+func (a *timeoutAgent) Run(ctx context.Context, opts agent.RunOpts) (*agent.Result, error) {
+	return invokeAgent(ctx, a.timeout, func(runCtx context.Context) (*agent.Result, error) {
+		return a.inner.Run(runCtx, opts)
+	})
+}
+
+func (a *timeoutAgent) SupportsSessionResume() bool {
+	return agent.SupportsSessionResume(a.inner)
+}
+
+func (a *timeoutAgent) SupportsSessionProvider(provider string) bool {
+	return agent.SupportsSessionProvider(a.inner, provider)
+}
+
+func (a *timeoutAgent) ReportsAgentAttempts() bool {
+	return agent.ReportsAgentAttempts(a.inner)
+}
+
+func (a *timeoutAgent) NeutralizesGateInstructions() bool {
+	return agent.NeutralizesGateInstructions(a.inner)
+}

internal/pipeline/agent_run_test.go
@@ -1,0 +1,186 @@
+package pipeline
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/kunchenguid/no-mistakes/internal/agent"
+	"github.com/kunchenguid/no-mistakes/internal/config"
+	"github.com/kunchenguid/no-mistakes/internal/types"
+)
+
+type hangingAgent struct {
+	name    string
+	runFn   func(ctx context.Context, opts agent.RunOpts) (*agent.Result, error)
+	calls   int
+	lastCtx context.Context
+}
+
+func (h *hangingAgent) Name() string { return h.name }
+
+func (h *hangingAgent) Close() error { return nil }
+
+func (h *hangingAgent) Run(ctx context.Context, opts agent.RunOpts) (*agent.Result, error) {
+	h.calls++
+	h.lastCtx = ctx
+	if h.runFn != nil {
+		return h.runFn(ctx, opts)
+	}
+	return &agent.Result{Text: "ok"}, nil
+}
+
+func TestRunAgent_HangingAgentFailsAfterTimeout(t *testing.T) {
+	t.Parallel()
+	ag := &hangingAgent{
+		name: "hang",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			<-ctx.Done()
+			return &agent.Result{Text: "late"}, nil
+		},
+	}
+	sctx := &StepContext{
+		Ctx:    context.Background(),
+		Agent:  ag,
+		Config: &config.Config{AgentTimeout: 20 * time.Millisecond},
+	}
+
+	start := time.Now()
+	_, err := sctx.RunAgent(agent.RunOpts{Prompt: "work"})
+	elapsed := time.Since(start)
+	if err == nil || !errors.Is(err, ErrAgentTimeout) {
+		t.Fatalf("error = %v, want ErrAgentTimeout", err)
+	}
+	if !strings.Contains(err.Error(), "agent timed out after 20ms") {
+		t.Fatalf("error = %q, want timeout diagnostic", err)
+	}
+	if elapsed > time.Second {
+		t.Fatalf("hung for %s, want a bounded fail", elapsed)
+	}
+}
+
+func TestRunAgent_LateSuccessAfterTimeoutIsRejected(t *testing.T) {
+	t.Parallel()
+	ag := &hangingAgent{
+		name: "late",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			<-ctx.Done()
+			return &agent.Result{Output: json.RawMessage(`{"summary":"should not ship"}`)}, nil
+		},
+	}
+	sctx := &StepContext{
+		Ctx:    context.Background(),
+		Agent:  ag,
+		Config: &config.Config{AgentTimeout: 20 * time.Millisecond},
+	}
+
+	result, err := sctx.RunAgent(agent.RunOpts{Prompt: "work"})
+	if err == nil || !errors.Is(err, ErrAgentTimeout) {
+		t.Fatalf("error = %v, want late-success timeout", err)
+	}
+	if result != nil {
+		t.Fatalf("result = %+v, want nil after timeout", result)
+	}
+}
+
+func TestRunAgent_SuccessfulOutputUnchanged(t *testing.T) {
+	t.Parallel()
+	want := json.RawMessage(`{"summary":"ok"}`)
+	ag := &hangingAgent{
+		name: "ok",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			if _, ok := ctx.Deadline(); !ok {
+				t.Fatal("successful invocation ran without a deadline")
+			}
+			if err := ctx.Err(); err != nil {
+				t.Fatalf("live invocation context: %v", err)
+			}
+			return &agent.Result{Output: want, Text: "ok"}, nil
+		},
+	}
+	sctx := &StepContext{
+		Ctx:    context.Background(),
+		Agent:  ag,
+		Config: &config.Config{AgentTimeout: time.Second},
+	}
+
+	result, err := sctx.RunAgent(agent.RunOpts{Prompt: "work"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result == nil || string(result.Output) != string(want) || result.Text != "ok" {
+		t.Fatalf("result = %+v, want original output", result)
+	}
+	if sctx.Ctx.Err() != nil {
+		t.Fatalf("parent context cancelled after successful run: %v", sctx.Ctx.Err())
+	}
+}
+
+func TestRunAgent_HonorsExistingSoonerDeadline(t *testing.T) {
+	t.Parallel()
+	parent, cancel := context.WithTimeout(context.Background(), 20*time.Millisecond)
+	defer cancel()
+	ag := &hangingAgent{
+		name: "parent-deadline",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			<-ctx.Done()
+			return &agent.Result{Text: "late"}, nil
+		},
+	}
+	sctx := &StepContext{
+		Ctx:    parent,
+		Agent:  ag,
+		Config: &config.Config{AgentTimeout: time.Hour},
+	}
+
+	_, err := sctx.RunAgent(agent.RunOpts{Prompt: "work"})
+	if err == nil {
+		t.Fatal("expected parent deadline to fail the invocation")
+	}
+	if errors.Is(err, ErrAgentTimeout) {
+		t.Fatalf("stacked default timeout onto an existing deadline: %v", err)
+	}
+	if !errors.Is(err, context.DeadlineExceeded) {
+		t.Fatalf("error = %v, want parent deadline", err)
+	}
+}
+
+func TestExecutor_DirectAgentRunIsDeadlineBounded(t *testing.T) {
+	database, p, run, repo := setupTest(t)
+	ag := &hangingAgent{
+		name: "hang",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			<-ctx.Done()
+			return &agent.Result{Text: "late"}, nil
+		},
+	}
+	step := &adaptiveCallStep{
+		name: types.StepDocument,
+		fn: func(sctx *StepContext) (*StepOutcome, error) {
+			_, err := sctx.Agent.Run(sctx.Ctx, agent.RunOpts{Prompt: "work"})
+			return nil, err
+		},
+	}
+	cfg := &config.Config{AgentTimeout: 20 * time.Millisecond}
+	exec := NewExecutor(database, p, cfg, ag, []Step{step}, nil)
+	if err := exec.Execute(context.Background(), run, repo, t.TempDir()); err == nil {
+		t.Fatal("expected hanging Agent.Run to fail the run")
+	}
+	got, err := database.GetRun(run.ID)
+	if err != nil {
+		t.Fatalf("get run: %v", err)
+	}
+	if got.Status != types.RunFailed {
+		t.Fatalf("run status = %s, want %s", got.Status, types.RunFailed)
+	}
+	if got.Error == nil || !strings.Contains(*got.Error, "agent timed out after 20ms") {
+		var msg string
+		if got.Error != nil {
+			msg = *got.Error
+		}
+		t.Fatalf("run error = %q, want timeout diagnostic", msg)
+	}
+}

Blakeolson21/no-slop

internal/pipeline/agent_run.go
@@ -1,0 +1,143 @@
+package pipeline
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/Blakeolson21/no-slop/internal/agent"
+	"github.com/Blakeolson21/no-slop/internal/config"
+)
+
+// ErrAgentTimeout is the context cause used when the default per-invocation
+// agent deadline expires. Callers wrap it with a diagnostic that names the
+// budget; a late successful return after this cause is still a timeout.
+var ErrAgentTimeout = errors.New("agent timeout")
+
+// AgentTimeout is the per-invocation budget applied at the shared agent-run
+// seam. A positive Config.AgentTimeout wins; otherwise the default (30m).
+func AgentTimeout(cfg *config.Config) time.Duration {
+	if cfg != nil && cfg.AgentTimeout > 0 {
+		return cfg.AgentTimeout
+	}
+	return config.DefaultAgentTimeout
+}
+
+// RunAgent executes one agent invocation with a deadline scoped only to that
+// call. The parent StepContext.Ctx is left unchanged so post-agent work
+// (commits, git, parsing) is not cancelled by the invocation budget.
+//
+// If the parent context already has a deadline (review's round budget, Test's
+// explicit wrap, intent extraction, caller cancellation), that bound is
+// honored and no shorter default is stacked. Otherwise AgentTimeout is
+// applied. A late successful return after the deadline is rejected.
+func (sctx *StepContext) RunAgent(opts agent.RunOpts) (*agent.Result, error) {
+	parent := context.Background()
+	if sctx != nil {
+		parent = sctx.Ctx
+	}
+	return sctx.runAgent(parent, opts, "")
+}
+
+// RunAgentContext is RunAgent with an explicit parent, used when a step has
+// already installed a more specific deadline (review round, Test invocation).
+func (sctx *StepContext) RunAgentContext(parent context.Context, opts agent.RunOpts) (*agent.Result, error) {
+	return sctx.runAgent(parent, opts, "")
+}
+
+// RunAgentSessionContext is RunAgentSession with an explicit parent so a
+// fixer turn can share a round budget (review) or a per-invocation wrap (Test).
+func (sctx *StepContext) RunAgentSessionContext(parent context.Context, role SessionRole, opts agent.RunOpts) (*agent.Result, error) {
+	return sctx.runAgent(parent, opts, role)
+}
+
+func (sctx *StepContext) runAgent(parent context.Context, opts agent.RunOpts, sessionRole SessionRole) (*agent.Result, error) {
+	var ag agent.Agent
+	timeout := AgentTimeout(nil)
+	if sctx != nil {
+		ag = sctx.Agent
+		timeout = AgentTimeout(sctx.Config)
+	}
+	return invokeAgent(parent, timeout, func(ctx context.Context) (*agent.Result, error) {
+		if sessionRole != "" && sctx != nil && sctx.Sessions != nil {
+			return sctx.Sessions.Run(ctx, ag, sessionRole, opts, sctx.Log)
+		}
+		if ag == nil {
+			return nil, errors.New("nil agent")
+		}
+		return ag.Run(ctx, opts)
+	})
+}
+
+func invokeAgent(parent context.Context, timeout time.Duration, run func(context.Context) (*agent.Result, error)) (*agent.Result, error) {
+	ctx, cancel, applied := bindAgentDeadline(parent, timeout)
+	result, err := run(ctx)
+	runErr := classifyAgentRun(ctx, applied, err)
+	cancel()
+	if runErr != nil {
+		return nil, runErr
+	}
+	return result, nil
+}
+
+func bindAgentDeadline(parent context.Context, timeout time.Duration) (context.Context, context.CancelFunc, time.Duration) {
+	if parent == nil {
+		parent = context.Background()
+	}
+	if timeout <= 0 {
+		return parent, func() {}, 0
+	}
+	if _, ok := parent.Deadline(); ok {
+		return parent, func() {}, 0
+	}
+	ctx, cancel := context.WithTimeoutCause(parent, timeout, ErrAgentTimeout)
+	return ctx, cancel, timeout
+}
+
+func classifyAgentRun(ctx context.Context, applied time.Duration, err error) error {
+	if applied > 0 && errors.Is(context.Cause(ctx), ErrAgentTimeout) {
+		return fmt.Errorf("agent timed out after %s (agent silent for %s): %w", applied, applied, ErrAgentTimeout)
+	}
+	if cause := context.Cause(ctx); cause != nil {
+		return cause
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// timeoutAgent is the executor backstop: every sctx.Agent.Run is bounded even
+// if a future step forgets RunAgent. Nested with RunAgent it is a no-op when
+// the incoming context already has a deadline.
+type timeoutAgent struct {
+	inner   agent.Agent
+	timeout time.Duration
+}
+
+func (a *timeoutAgent) Name() string { return a.inner.Name() }
+
+func (a *timeoutAgent) Close() error { return a.inner.Close() }
+
+func (a *timeoutAgent) Run(ctx context.Context, opts agent.RunOpts) (*agent.Result, error) {
+	return invokeAgent(ctx, a.timeout, func(runCtx context.Context) (*agent.Result, error) {
+		return a.inner.Run(runCtx, opts)
+	})
+}
+
+func (a *timeoutAgent) SupportsSessionResume() bool {
+	return agent.SupportsSessionResume(a.inner)
+}
+
+func (a *timeoutAgent) SupportsSessionProvider(provider string) bool {
+	return agent.SupportsSessionProvider(a.inner, provider)
+}
+
+func (a *timeoutAgent) ReportsAgentAttempts() bool {
+	return agent.ReportsAgentAttempts(a.inner)
+}
+
+func (a *timeoutAgent) NeutralizesGateInstructions() bool {
+	return agent.NeutralizesGateInstructions(a.inner)
+}

internal/pipeline/agent_run_test.go
@@ -1,0 +1,186 @@
+package pipeline
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/Blakeolson21/no-slop/internal/agent"
+	"github.com/Blakeolson21/no-slop/internal/config"
+	"github.com/Blakeolson21/no-slop/internal/types"
+)
+
+type hangingAgent struct {
+	name    string
+	runFn   func(ctx context.Context, opts agent.RunOpts) (*agent.Result, error)
+	calls   int
+	lastCtx context.Context
+}
+
+func (h *hangingAgent) Name() string { return h.name }
+
+func (h *hangingAgent) Close() error { return nil }
+
+func (h *hangingAgent) Run(ctx context.Context, opts agent.RunOpts) (*agent.Result, error) {
+	h.calls++
+	h.lastCtx = ctx
+	if h.runFn != nil {
+		return h.runFn(ctx, opts)
+	}
+	return &agent.Result{Text: "ok"}, nil
+}
+
+func TestRunAgent_HangingAgentFailsAfterTimeout(t *testing.T) {
+	t.Parallel()
+	ag := &hangingAgent{
+		name: "hang",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			<-ctx.Done()
+			return &agent.Result{Text: "late"}, nil
+		},
+	}
+	sctx := &StepContext{
+		Ctx:    context.Background(),
+		Agent:  ag,
+		Config: &config.Config{AgentTimeout: 20 * time.Millisecond},
+	}
+
+	start := time.Now()
+	_, err := sctx.RunAgent(agent.RunOpts{Prompt: "work"})
+	elapsed := time.Since(start)
+	if err == nil || !errors.Is(err, ErrAgentTimeout) {
+		t.Fatalf("error = %v, want ErrAgentTimeout", err)
+	}
+	if !strings.Contains(err.Error(), "agent timed out after 20ms") {
+		t.Fatalf("error = %q, want timeout diagnostic", err)
+	}
+	if elapsed > time.Second {
+		t.Fatalf("hung for %s, want a bounded fail", elapsed)
+	}
+}
+
+func TestRunAgent_LateSuccessAfterTimeoutIsRejected(t *testing.T) {
+	t.Parallel()
+	ag := &hangingAgent{
+		name: "late",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			<-ctx.Done()
+			return &agent.Result{Output: json.RawMessage(`{"summary":"should not ship"}`)}, nil
+		},
+	}
+	sctx := &StepContext{
+		Ctx:    context.Background(),
+		Agent:  ag,
+		Config: &config.Config{AgentTimeout: 20 * time.Millisecond},
+	}
+
+	result, err := sctx.RunAgent(agent.RunOpts{Prompt: "work"})
+	if err == nil || !errors.Is(err, ErrAgentTimeout) {
+		t.Fatalf("error = %v, want late-success timeout", err)
+	}
+	if result != nil {
+		t.Fatalf("result = %+v, want nil after timeout", result)
+	}
+}
+
+func TestRunAgent_SuccessfulOutputUnchanged(t *testing.T) {
+	t.Parallel()
+	want := json.RawMessage(`{"summary":"ok"}`)
+	ag := &hangingAgent{
+		name: "ok",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			if _, ok := ctx.Deadline(); !ok {
+				t.Fatal("successful invocation ran without a deadline")
+			}
+			if err := ctx.Err(); err != nil {
+				t.Fatalf("live invocation context: %v", err)
+			}
+			return &agent.Result{Output: want, Text: "ok"}, nil
+		},
+	}
+	sctx := &StepContext{
+		Ctx:    context.Background(),
+		Agent:  ag,
+		Config: &config.Config{AgentTimeout: time.Second},
+	}
+
+	result, err := sctx.RunAgent(agent.RunOpts{Prompt: "work"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result == nil || string(result.Output) != string(want) || result.Text != "ok" {
+		t.Fatalf("result = %+v, want original output", result)
+	}
+	if sctx.Ctx.Err() != nil {
+		t.Fatalf("parent context cancelled after successful run: %v", sctx.Ctx.Err())
+	}
+}
+
+func TestRunAgent_HonorsExistingSoonerDeadline(t *testing.T) {
+	t.Parallel()
+	parent, cancel := context.WithTimeout(context.Background(), 20*time.Millisecond)
+	defer cancel()
+	ag := &hangingAgent{
+		name: "parent-deadline",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			<-ctx.Done()
+			return &agent.Result{Text: "late"}, nil
+		},
+	}
+	sctx := &StepContext{
+		Ctx:    parent,
+		Agent:  ag,
+		Config: &config.Config{AgentTimeout: time.Hour},
+	}
+
+	_, err := sctx.RunAgent(agent.RunOpts{Prompt: "work"})
+	if err == nil {
+		t.Fatal("expected parent deadline to fail the invocation")
+	}
+	if errors.Is(err, ErrAgentTimeout) {
+		t.Fatalf("stacked default timeout onto an existing deadline: %v", err)
+	}
+	if !errors.Is(err, context.DeadlineExceeded) {
+		t.Fatalf("error = %v, want parent deadline", err)
+	}
+}
+
+func TestExecutor_DirectAgentRunIsDeadlineBounded(t *testing.T) {
+	database, p, run, repo := setupTest(t)
+	ag := &hangingAgent{
+		name: "hang",
+		runFn: func(ctx context.Context, _ agent.RunOpts) (*agent.Result, error) {
+			<-ctx.Done()
+			return &agent.Result{Text: "late"}, nil
+		},
+	}
+	step := &adaptiveCallStep{
+		name: types.StepDocument,
+		fn: func(sctx *StepContext) (*StepOutcome, error) {
+			_, err := sctx.Agent.Run(sctx.Ctx, agent.RunOpts{Prompt: "work"})
+			return nil, err
+		},
+	}
+	cfg := &config.Config{AgentTimeout: 20 * time.Millisecond}
+	exec := NewExecutor(database, p, cfg, ag, []Step{step}, nil)
+	if err := exec.Execute(context.Background(), run, repo, t.TempDir()); err == nil {
+		t.Fatal("expected hanging Agent.Run to fail the run")
+	}
+	got, err := database.GetRun(run.ID)
+	if err != nil {
+		t.Fatalf("get run: %v", err)
+	}
+	if got.Status != types.RunFailed {
+		t.Fatalf("run status = %s, want %s", got.Status, types.RunFailed)
+	}
+	if got.Error == nil || !strings.Contains(*got.Error, "agent timed out after 20ms") {
+		var msg string
+		if got.Error != nil {
+			msg = *got.Error
+		}
+		t.Fatalf("run error = %q, want timeout diagnostic", msg)
+	}
+}