kunchenguid/no-mistakes

no-mistakes(document): Document validated delivery resume behavior

internal/skill/skill.go
@@ -260,8 +260,10 @@ the branch itself**; a PR that is merely behind but still clean needs nothing
 either, since the platform merges it. The one exception is when that monitor is
 no longer running - the PR was closed, the run was aborted or superseded, it
 idle-timed-out, or its auto-fix attempts were exhausted - in which case recover
-with ` + "`no-mistakes rerun`" + `, which cancels the stale monitor and re-runs the full
-pipeline including a deterministic rebase step. Do **not** reach for
+with ` + "`no-mistakes rerun`" + `. When the failed run has an exact, mechanically
+verified post-validation delivery checkpoint, the rerun resumes at Push without
+repeating Review, Test, or Document; otherwise it fails closed to the full
+pipeline, including a deterministic rebase step. Do **not** reach for
 ` + "`no-mistakes axi run`" + ` to refresh a still-active PR: after ` + "`checks-passed`" + ` it
 reattaches to the running monitor (HEAD unchanged) and returns its output
 without rebasing.

Blakeolson21/no-slop

internal/skill/skill.go
@@ -274,8 +274,10 @@ validation at Review, and publishes it through Push**; a PR that is merely behin
 either, since the platform merges it. The one exception is when that monitor is
 no longer running - the PR was closed, the run was aborted or superseded, it
 idle-timed-out, or its auto-fix attempts were exhausted - in which case recover
-with ` + "`no-slop rerun`" + `, which cancels the stale monitor and re-runs the full
-pipeline including a deterministic rebase step. Do **not** reach for
+with ` + "`no-slop rerun`" + `. When the failed run has an exact, mechanically
+verified post-validation delivery checkpoint, the rerun resumes at Push without
+repeating Review, Test, or Document; otherwise it fails closed to the full
+pipeline, including a deterministic rebase step. Do **not** reach for
 ` + "`no-slop axi run`" + ` to refresh a still-active PR: after ` + "`checks-passed`" + ` it
 reattaches to the running monitor (HEAD unchanged) and returns its output
 without rebasing.